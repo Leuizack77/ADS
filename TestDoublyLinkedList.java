71100a7e1a85d02c97672688b12558d5103a80b9
Abhishek Alkari <147973324+Leuizack77@users.noreply.github.com>
2025-11-28 00:27:15 +0530

    Create TestDoublyLinkedList.java

---
+++ TestDoublyLinkedList.java
@@ -0,0 +1,16 @@
+
+public class TestDoublyLinkedList {
+	public static void main(String args[]) {
+		DoublyLinkedList list = new DoublyLinkedList();
+		
+		list.addNode(10);
+		
+		list.addByPos(2, 20);
+		list.addByPos(3, 30);
+		list.addByPos(4, 40);
+		list.addByPos(5, 50);
+		
+		list.addAfterValue(40, 45);
+		list.display();
+	}
+}
